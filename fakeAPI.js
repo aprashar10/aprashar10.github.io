7793a93f6e0471e80f5f5832ffbe29b171c30f97
aprashar10 <95385501+aprashar10@users.noreply.github.com>
2021-12-01 14:46:24 -0800

    Updated fakeAPI.js and added exampleJSON.txt

--- fakeAPI.js
+++ fakeAPI.js
@@ -1,11 +1,21 @@
 "use strict";
 
+
+// Get the current URL
 let urlAppend = window.location.href;
 
-urlAppend = urlAppend.split("github.io").pop();
+
+// Split the URL at the end of the site name and keep only the latter halve
+urlAppend = urlAppend.split("github.io/").pop();
 
 
+// Update the <p> element with this portion of the string
 document.getElementById("test").innerHTML = urlAppend;
 
 
+if (urlAppend.length == 0) {
+  console.log(":(")
+} else {
+  console.log(":)")
+}
 
